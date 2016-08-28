860e60a46a53e627573017e6453222d39fa9edc3
Egon Elbre <egonelbre@gmail.com>
2016-08-28 20:50:52 +0300

    Basic game logic.

---
+++ Assets/World/Util.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+	public static class Rand
+	{
+		private static Random rng = new Random ();
+
+		public static Card Pick (List<Card> cards)
+		{
+			int index = rng.Next (cards.Count);
+			Card card = cards [index];
+			cards.RemoveAt (index);
+			return card;
+		}
+	}
+}
