44ceb3751e07d90863cda523a31e2566b7fd224a
Egon Elbre <egonelbre@gmail.com>
2016-09-02 08:32:39 +0300

    Add random rotation to target position.

--- Assets/Card/CardAnimator.cs
+++ Assets/Card/CardAnimator.cs
@@ -45,6 +45,14 @@ public class CardAnimator : MonoBehaviour
 			pos.rotation = Quaternion.Euler (new Vector3 (rot.x, rot.y + amount, rot.z));
 			return pos;
 		}
+
+		public Position RotateZ (float amount)
+		{
+			Position pos = new Position (this);
+			Vector3 rot = rotation.eulerAngles;
+			pos.rotation = Quaternion.Euler (new Vector3 (rot.x, rot.y, rot.z + amount));
+			return pos;
+		}
 	}
 
 	private class Animation
@@ -73,6 +81,8 @@ public class CardAnimator : MonoBehaviour
 		{
 			start = new Position (card);
 			stop = targets [(int)state];
+
+			stop.rotation = Quaternion.Slerp (stop.rotation, Random.rotation, 0.01f);
 		}
 
 		public void Stop ()
