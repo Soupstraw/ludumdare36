921c690ea21304e9a72dd563ba52cb4769af21a5
Egon Elbre <egonelbre@gmail.com>
2016-09-02 12:53:15 +0300

    Add background music.

--- Assets/BackgroundMusic.cs
+++ Assets/BackgroundMusic.cs
@@ -1,22 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BackgroundMusic
+public class BackgroundMusic : MonoBehaviour
 {
 	public AudioClip[] tracks;
-	public int activeTrack = 0;
+	private int activeTrack = -1;
 
-	public AnimationCurve interpolationCurve;
-	public float interpolationTime = 3f;
+	public AnimationCurve crossfadeCurve;
+	public float crossfadeDuration = 3f;
+	[Range (0f, 1f)] private float crossfade = 0.0f;
 
 	[Range (0f, 1f)] public float volume = 1f;
 
-	private AudioSource main;
+	private AudioSource primary;
 	private AudioSource secondary;
 
-	public BackgroundMusic ()
+	void Start ()
 	{
-		
+		primary = gameObject.AddComponent<AudioSource> ();
+		primary.loop = true;
+
+		secondary = gameObject.AddComponent<AudioSource> ();
+		secondary.loop = true;
+
+		InvokeRepeating ("Next", 0f, 45f);
+	}
+
+	void Next ()
+	{
+		activeTrack = (activeTrack + 1) % tracks.Length;
+		ChangeTo (tracks [activeTrack]);
+	}
+
+	void ChangeTo (AudioClip song)
+	{
+		secondary.clip = song;
+		secondary.volume = 0.0f;
+		secondary.time = 0.0f;
+		secondary.Play ();
+
+		crossfade = 0.0f;
+	}
+
+	void Update ()
+	{
+		if (secondary.clip == null) {
+			return;
+		}
+		crossfade += Time.deltaTime / crossfadeDuration;
+
+		float p = crossfadeCurve.Evaluate (crossfade);
+		p = Mathf.Clamp01 (p);
+
+		primary.volume = (1.0f - p) * volume;
+		secondary.volume = (p) * volume;
+
+		if (p >= 1.0f) {
+			AudioSource tmp = secondary;
+			secondary = primary;
+			primary = tmp;
+
+			secondary.Stop ();
+			secondary.time = 0.0f;
+			secondary.volume = 0.0f;
+			secondary.clip = null;
+		}
 	}
 }
 
